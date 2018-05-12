2aff66e2abfa903d35a55112938958e01870a6ca
ckrgithub <13560177212@163.com>
2018-05-12 21:10:35 +0800

    1.抽取tabLayout布局

--- app/src/main/java/com/ckr/mediabrowser/view/PhotoFragment.java
+++ app/src/main/java/com/ckr/mediabrowser/view/PhotoFragment.java
@@ -12,9 +12,9 @@ import com.ckr.mediabrowser.R;
 public class PhotoFragment extends BaseFragment {
 
 	public static PhotoFragment newInstance() {
-		
+
 		Bundle args = new Bundle();
-		
+
 		PhotoFragment fragment = new PhotoFragment();
 		fragment.setArguments(args);
 		return fragment;
